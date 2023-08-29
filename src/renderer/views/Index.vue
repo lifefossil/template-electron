d3524984c292649e2ed3c3ac1b60babe87079104
oliver <lifefossil@outlook.com>
2023-08-29 21:35:43 +0800

    init project

---
+++ src/renderer/views/Index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import HelloWorld from '@/components/HelloWorld.vue'
+</script>
+
+<template>
+  <main>
+    <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <HelloWorld></HelloWorld>
+  </main>
+</template>
